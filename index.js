5d976f0bd983372c3bc0682a85d804a1fe6c3844
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-26 23:57:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
-const simpleGit = require('simple-git');
+const simpleGit = require('simple-git')
 const FILE_PATH = 'C:\Users\Tushar Mishra\Json\Json\data.json'
 const DATE = moment().subtract(1,'d').format()
 const data = {
