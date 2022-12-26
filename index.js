c267fb7d095287d6058f1687cc8e7d7deaf67207
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-26 23:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const DATE = moment().subtract(1,'d').format()
 const data = {
     date : DATE
 }  
-jsonfile.writeFile(FILE_PATH,data);
+jsonfile.writeFile(FILE_PATH,data)
 simpleGit().add([FILE_PATH]).commit(DATE,{'--date': DATE}).push()
