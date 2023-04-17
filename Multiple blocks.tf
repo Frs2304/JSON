aab5fa42d8d35979b7eba442ef7a38695b04f894
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-04-17 23:51:06 +0530

    Initial commit

---
+++ Multiple blocks.tf
@@ -0,0 +1,12 @@
+// First output block
+output "firstoutputblock" {
+    value = "this is first"
+}
+// Second output block
+output "secondoutputblock" {
+    value = "this is second"
+}
+// Third output block
+output "thirdoutputblock" {
+    value = "this is third"
+}
